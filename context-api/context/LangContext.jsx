158c6a2e96f8e326fed38ffc806a78882e0c14e8
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-03-09 21:14:39 +0530

    html font changer using conetext api

--- context-api/context/LangContext.jsx
+++ context-api/context/LangContext.jsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 const LangContext=createContext(undefined);
 export const LangProvider=({children})=>{
     const [lang,setLang]=useState('en');
-    
+
     const toggleLang=()=>{
         const newLang=lang==='en'?'fr':'en';
-        document.getElementsByTagName("html").setAttribute("lang","newLang");
+        document.documentElement.setAttribute("lang",newLang);
         setLang(newLang)
     }
     return(
